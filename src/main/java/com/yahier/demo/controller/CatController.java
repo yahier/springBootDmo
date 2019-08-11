1957a8ca2afcfdd744bcecf16ebbcf5c190d0f9e
xuziwang <xuziwang@xhg.com>
2019-08-11 17:12:19 +0800

    。。。我是太笨了 。添加@RequestBody postman的header修改对应的content-Type

--- src/main/java/com/yahier/demo/controller/CatController.java
+++ src/main/java/com/yahier/demo/controller/CatController.java
@@ -5,13 +5,16 @@ import com.yahier.demo.entity.BaseResp;
 import com.yahier.demo.service.CatService;
 import com.yahier.demo.table.Cat;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * tip:好久都没有想到去设置postman中 header下的Content-Type，导致模拟请求不是很顺畅
+ */
 @RestController
 @RequestMapping("/cat")
 public class CatController {
@@ -19,21 +22,24 @@ public class CatController {
     @Autowired
     private CatService catService;
 
+    /**
+     * 单个添加
+     */
     @RequestMapping(value = "/add", produces = {"application/json;charset=UTF-8"}, method = RequestMethod.POST)
-    public BaseResp add(Cat cat) {
+    public BaseResp add(@RequestBody Cat cat) {
         StaticLog.info("猫的type is {}", cat.getType());
         catService.save(cat);
         return new BaseResp(cat);
     }
 
 
+    /**
+     * 列表添加
+     */
     @RequestMapping(value = "/addList", produces = {"application/json;charset=UTF-8"}, method = RequestMethod.POST)
-    public BaseResp addList(Cat cat) {
-        List<Cat> listCat = new ArrayList<>();
-        listCat.add(new Cat());
-        listCat.add(new Cat());
+    public BaseResp addList(@RequestBody List<Cat> listCat) {
         catService.saveBatch(listCat);
-        return new BaseResp(true);
+        return new BaseResp(listCat);
     }
 
 }
